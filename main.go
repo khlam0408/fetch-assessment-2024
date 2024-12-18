c8ab4abd48e1ec65c76d1579886fd2afc77aae18
Kelly Lam <khlam@buffalo.edu>
2024-12-18 16:04:57 -0500

    Added comments for the main.go file

--- main.go
+++ main.go
@@ -10,11 +10,12 @@ import(
 var idsToPoints = make(map[string]int)
 
 func postID(c *gin.Context){
-	newUUID := uuid.New()
+	newUUID := uuid.New() // the ID for the receipt
 	var currReceipt models.Receipt
 
 	err := c.BindJSON(&currReceipt)
 
+	// If it cannot bind to the receipt, a error message will be sent
 	if err != nil{
 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "The receipt is invalid"})
 		return
@@ -24,29 +25,33 @@ func postID(c *gin.Context){
 
 	idsToPoints[newUUID.String()] = points
 	
-	c.IndentedJSON(http.StatusCreated, gin.H{"id": newUUID.String()})
+	c.IndentedJSON(http.StatusCreated, gin.H{"id": newUUID.String()}) // response with a JSON object
 }
 
 
 func getPoints(c *gin.Context){
 	id := c.Param("id")
+	// goes through a list of all ids that has been inputed via the POST route
 	for i, p := range idsToPoints{
         if i == id {
             c.IndentedJSON(http.StatusOK, gin.H{"points": p})
             return
         }
     }
+	// If there is no such ID (not being placed via the POST), will respond with an error message 
     c.IndentedJSON(http.StatusNotFound, gin.H{"message": "No receipt found for that ID"})
 	
 }
 
-
+// Router set up for all the necessary routes
 func SetUpRouter() *gin.Engine{
 	router := gin.Default()
 	router.POST("/receipts/process", postID)
 	router.GET("/receipts/:id/points", getPoints)
 	return router
 }
+
+// Runs the router
 func main(){
 	r := SetUpRouter()
 	r.Run(":8080")
